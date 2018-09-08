e7c215ee3971658fd264a89337592f2715ffa46f
wendelmax <jacksonwendel@gmail.com>
2018-09-07 23:46:58 -0300

    Subindo projeto local

---
+++ Controllers/IndicadoresController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+
+
+namespace APIIndicadores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IndicadoresController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Indicador> Get(
+            [FromServices]IConfiguration config)
+        {
+            using (SqlConnection conexao =
+            new SqlConnection(config.GetConnectionString("BaseIndicadores")))
+            {
+                return conexao.Query<Indicador>("SELECT * FROM dbo.Indicadores");
+            }
+        }
+
+    }
+}
---
+++ Indicador.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace APIIndicadores
+{
+    public class Indicador
+    {
+        public string Sigla{get; set;}
+        public string NomeIndicador{get; set;}
+        public DateTime UltimaAtualizacao{get; set;}
+        public decimal Valor{get;set;}
+
+    }
+}
